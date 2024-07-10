b80d544056636e5223e20552297afe32441b91f5
Fiodor Gherasimenco <fgherasimenco@Fiodors-MacBook-Pro.local>
2024-07-10 22:31:31 +0300

    fix: notice mobile layout

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -10,7 +10,7 @@ export const Notice = ({ message, onHide }: Props) => {
   return (
     <div className={style.notice}>
       <p>{message}</p>
-      <div>
+      <div className={style.controls}>
         <Button size="small" onClick={onHide}>
           Dismiss
         </Button>
