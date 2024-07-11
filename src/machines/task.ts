d8fc731e52b4d2de3e2d84c2a9e3b3ff49f7eb1c
Fiodor Gherasimenco <fgherasimenco@Fiodors-MacBook-Pro.local>
2024-07-11 14:41:09 +0300

    feat: remove task in error state

--- src/machines/task.ts
+++ src/machines/task.ts
@@ -108,6 +108,9 @@ export default () => {
           switch: {
             target: States.Idle,
           },
+          delete: {
+            target: States.Hide,
+          },
         },
       },
     },
